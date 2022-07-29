557fc70242aaa2857da3eb2283524eb26e9962c1
Armen Marjinyan <armenmarjinyan@MacBook-Pro-Armen.local>
2022-07-29 21:12:01 +0400

    initial

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {useState, useEffect} from 'react'
+import Grid from './Grid/Grid'
+import Table from './Table/Table'
+
+const App = () => {
+  const[users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api?results=9&inc=name,picture")
+    .then(r => r.json())
+    .then(r => setUsers(r.results))
+  }, [])
+
+  return <div>
+    <Grid users={users} />
+    <Table users={users} />
+  </div>
+}
+
+export default App
